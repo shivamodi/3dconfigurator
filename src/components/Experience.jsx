fc1cf0af9ba0f136295eea75f25d3cfa27d5d577
Shiva Modi <shivamodi@gmail.com>
2023-08-08 14:15:39 +0400

    further changes

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -13,7 +13,7 @@ const Experience = () => {
         global={false} // Spin globally or by dragging the model
         cursor={true} // Whether to toggle cursor style on drag
         snap={false} // Snap-back to center (can also be a spring config)
-        speed={1.5}
+        speed={1}
         rotation={[0, 0, 0]} 
       >
         <Stage environment={null} intensity={1}  shadows="false">
