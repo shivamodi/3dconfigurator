0acfcffbe0b6395857907feb5b2d65767ef914e6
root <root@instance-1.me-central1-a.c.precise-ratio-395312.internal>
2023-08-11 13:24:03 +0000

    Revert "code changes from server"
    This reverts commit 542ed089848c83ec5077fdc693d5d14617a540dc.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./components/Experience";
 import Configurator from "./components/Configurator";
 import { CustomizationProvider } from "./contexts/Customization";
-import { Suspense } from 'react'
 
 function App() {
 
-return (
+  return (
     <CustomizationProvider>
       <div className="App">
         <Canvas dpr={[1, 2]} shadows="false">
@@ -21,6 +20,5 @@ return (
     </CustomizationProvider>
   );
 }
-
 
 export default App;
